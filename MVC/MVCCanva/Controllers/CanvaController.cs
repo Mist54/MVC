e4130c189d2ec5d4dfc01f0790ca745906471e78
Mist54 <sankalpmshet09@gmail.com>
2025-02-26 21:08:12 +0530

    Initial push

---
+++ MVC/MVCCanva/Controllers/CanvaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCanva.Controllers
+{
+    public class CanvaController : Controller
+    {
+        // GET: Canva
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
